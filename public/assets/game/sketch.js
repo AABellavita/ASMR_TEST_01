e08fdc4b3a397ece49524939ba2264af6607337f
Andrea Bellavita <andrea.bellavita@mail.polimi.it>
2021-01-15 12:11:34 +0100

    Update sketch.js

--- public/assets/game/sketch.js
+++ public/assets/game/sketch.js
@@ -164,7 +164,7 @@ socket.on("current", function (data) {
 });
 
 socket.on("playsong", function (data) {
-  audio.currentTime = data - 0.25;
+  audio.currentTime = data - 0.5;
   console.log(audio.currentTime);
   audio.play();
 });
